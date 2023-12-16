c0e10fd6e8ce1f7969123d79e2453d93abfe7a9f
nenadV91 <nenadwebdevelop@gmail.com>
2023-12-16 20:09:29 +0100

    feat: added update and delete user

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,12 +8,16 @@ const filterObj = (obj, ...allowedFields) =>
     return r;
   }, {});
 
-exports.getAllUsers = (req, res) => {
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find({});
+
   res.status(500).send({
     status: 'error',
-    message: 'This route is not yet defined',
+    data: {
+      users,
+    },
   });
-};
+});
 exports.createUser = (req, res) => {
   res.status(500).send({
     status: 'error',
@@ -64,5 +68,14 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     data: {
       user: updatedUser,
     },
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
--- models/userModel.js
+++ models/userModel.js
@@ -44,6 +44,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre('save', async function (next) {
@@ -66,6 +71,12 @@ userSchema.pre('save', function (next) {
   }
 
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre(/^find/, function (next) {
+  // This points to current query
+  this.find({ active: { $ne: false } });
   next();
 });
 
