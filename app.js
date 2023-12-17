387bed1e631723eb6d05b2dc7dc204299b85d7a6
nenadV91 <nenadwebdevelop@gmail.com>
2023-12-17 15:23:52 +0100

    feat: added security middleware

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRouter');
@@ -9,10 +12,15 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
+// Set ecurity HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -20,15 +28,30 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from the body into req.body
+app.use(
+  express.json({
+    limit: '10kb',
+  }),
+);
+
+// Data sanitization against NoSQL injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
   next();
 });
 
+// Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
